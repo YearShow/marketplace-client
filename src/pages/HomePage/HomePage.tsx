f6fbcce16428289acae9af4216a6e39d827ebbbb
Alexander Ershov <yearshow@yandex.ru>
2022-10-08 08:40:02 +0300

    Replace from other repo

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet";
+import { TestDiv } from './styled';
+
+const HomePage: React.FC = () => {
+	return <>
+		<Helmet>
+			<title>Главная - MW Marketplace</title>
+		</Helmet>
+	</>
+}
+
+export default HomePage;
